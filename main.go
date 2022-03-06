9dca61e92c2821d091f0c10770e4b61e2a4151df
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-03-06 17:25:19 +0100

    feat: add Get-Endpoint (#2)

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"net/http"
 
+	"go-service-v2/internal/albums"
+
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	r := mux.NewRouter()
+	getRouter := r.Methods("GET").Subrouter()
+
+	getRouter.HandleFunc("/", albums.ListAlbums)
 	http.ListenAndServe(":9090", r)
 }
