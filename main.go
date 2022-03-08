9a41809f6d00abe370df627bcd8dc0619745144c
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-03-08 17:59:25 +0100

    feat: add post endpoint (#6)

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 func main() {
 	r := mux.NewRouter()
 	getRouter := r.Methods("GET").Subrouter()
+	postRouter := r.Methods("POST").Subrouter()
+
+	postRouter.HandleFunc("/albums", handlers.AddAlbum)
 
 	getRouter.HandleFunc("/", handlers.ListAlbums)
 	http.ListenAndServe(":9090", r)
