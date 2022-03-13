86e45a0cc8355dbf9ae3ab1d584682dc7301df78
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-03-13 12:14:55 +0000

    feat: init client

--- album-service/main.go
+++ album-service/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 
 	"github.com/cahllagerfeld/go-service-v2/album-service/albums/handlers"
+	protos "github.com/cahllagerfeld/go-service-v2/number-service/proto"
 	"github.com/gorilla/mux"
+	"google.golang.org/grpc"
+)
+
+var (
+	addr = flag.String("addr", "localhost:9091", "the address to connect to")
 )
 
 func main() {
+	flag.Parse()
+
+	conn, err := grpc.Dial("localhost:9091", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+
 	r := mux.NewRouter()
 	getRouter := r.Methods(http.MethodGet).Subrouter()
 	postRouter := r.Methods(http.MethodPost).Subrouter()
 	deleteRouter := r.Methods(http.MethodDelete).Subrouter()
 	putRouter := r.Methods(http.MethodPut).Subrouter()
+
+	nc := protos.NewNumberClient(conn)
 
 	postRouter.HandleFunc("/albums", handlers.AddAlbum)
 
