b5df5e36a865cca60f35647520dc12c63a5c3318
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-03-16 18:49:03 +0000

    feat: rewrite gateway readall & create

--- gateway/albums/handlers/albums.go
+++ gateway/albums/handlers/albums.go
@@ -1,17 +1,17 @@
 package handlers
 
 import (
+	"context"
+	"encoding/json"
 	"net/http"
 	"strconv"
 
 	albumProtos "github.com/cahllagerfeld/go-service-v2/album/proto"
 	"github.com/cahllagerfeld/go-service-v2/gateway/albums/data"
-	numberProtos "github.com/cahllagerfeld/go-service-v2/number/proto"
 	"github.com/gorilla/mux"
 )
 
 type Albums struct {
-	nc numberProtos.NumberClient
 	ac albumProtos.AlbumClient
 }
 
@@ -20,39 +20,35 @@ func NewAlbums(ac albumProtos.AlbumClient) *Albums {
 }
 
 func (a *Albums) ListAlbums(rw http.ResponseWriter, r *http.Request) {
+
+	resp, err := a.ac.GetAllAlbums(context.Background(), &albumProtos.GetAllAlbumsRequest{})
+	if err != nil {
+		http.Error(rw, "Failed to get all Albums", http.StatusInternalServerError)
+	}
+
 	rw.Header().Set("Content-Type", "application/json")
 	rw.WriteHeader(http.StatusOK)
-
-	albums := data.GetAlbums()
-
-	err := albums.ToJson(rw)
-
-	if err != nil {
-		http.Error(rw, "Failed to parse Albums", http.StatusInternalServerError)
-	}
+	json.NewEncoder(rw).Encode(resp.Albums)
 
 }
 
 func (a *Albums) AddAlbum(rw http.ResponseWriter, r *http.Request) {
-
-	rw.WriteHeader(http.StatusCreated)
 	var album data.AlbumRequest
-	err := album.FromJson(r.Body)
+	err := json.NewDecoder(r.Body).Decode(&album)
 
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusBadRequest)
 	}
-	data, err := data.AddAlbum(&album, a.nc)
+	resp, err := a.ac.CreateAlbum(context.Background(), &albumProtos.CreateAlbumRequest{Title: album.Title, Artist: album.Artist})
 
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 	}
 
-	err = data.ToJson(rw)
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+	json.NewEncoder(rw).Encode(resp)
 
-	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-	}
 }
 
 func (a *Albums) RemoveAlbum(rw http.ResponseWriter, r *http.Request) {
