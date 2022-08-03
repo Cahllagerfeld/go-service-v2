d1b5d5489305a37b29932002abb596375716ea0c
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-08-03 20:08:53 +0000

    feat: add function to service

--- discovery/internal/core/services/registersrv.go
+++ discovery/internal/core/services/registersrv.go
@@ -1,6 +1,8 @@
 package registersrv
 
 import (
+	"fmt"
+
 	"github.com/cahllagerfeld/go-service-v2/discovery/internal/core/domain"
 	"github.com/cahllagerfeld/go-service-v2/discovery/internal/ports"
 )
@@ -15,14 +17,29 @@ func New(repository ports.DiscoveryRepository) *Service {
 	}
 }
 
-func (serv *Service) register(service *domain.Service) (*domain.Service, error) {
-	return nil, nil
+func (serv *Service) register(service domain.Service) (*domain.Service, error) {
+	result, err := serv.repo.Save(service)
+	if err != nil {
+		err = fmt.Errorf("Failed to register service: %s", err)
+		return nil, err
+	}
+	return result, nil
 }
 
 func (serv *Service) getAll() ([]*domain.Service, error) {
-	return nil, nil
+	result, err := serv.repo.GetAll()
+	if err != nil {
+		err = fmt.Errorf("Failed to get all: %s", err)
+		return nil, err
+	}
+	return result, nil
 }
 
-func (serv *Service) unregister(id string) (*domain.Service, error) {
-	return nil, nil
+func (serv *Service) unregister(id string) error {
+	err := serv.repo.Delete(id)
+	if err != nil {
+		err = fmt.Errorf("Failed to delete: %s", err)
+		return err
+	}
+	return nil
 }
--- discovery/internal/ports/ports.go
+++ discovery/internal/ports/ports.go
@@ -3,7 +3,7 @@ package ports
 import "github.com/cahllagerfeld/go-service-v2/discovery/internal/core/domain"
 
 type DiscoveryRepository interface {
-	Save(domain.Service) error
+	Save(domain.Service) (*domain.Service, error)
 	Delete(id string) error
 	GetAll() ([]*domain.Service, error)
 }
